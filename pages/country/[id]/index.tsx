4b9e53c0b065b4edfed8248e22c57e0ee1c21e96
Farshid Karimi <farshidk49@gmail.com>
2022-03-31 00:28:28 +0430

    fourth commit

--- pages/country/[id]/index.tsx
+++ pages/country/[id]/index.tsx
@@ -4,14 +4,13 @@ import styles from '../../../styles/Home.module.css'
 
 const country  = ({country}:any) => {
 
-  if(country){
+
     return(
 
       <div>
-
       <Link href="/"><a className={styles.back_button}> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-  <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-</svg> Back</a></Link>
+      <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+      </svg> Back</a></Link>
       <div className={styles.single_page}>
       <img src={country[0].flags.svg} alt="" width="400" height="300"/>
 
@@ -39,13 +38,7 @@ const country  = ({country}:any) => {
 
       </div>
     );
-  }else{
-    return(
 
-      <p>Error Loading Data</p>
-
-    );
-  }
 }
 
 
