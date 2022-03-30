e3f25c85f3d0bb6a6d1ff441c15b281a6992d567
Farshid Karimi <farshidk49@gmail.com>
2022-03-31 00:14:48 +0430

    second commit

--- pages/country/[id]/index.tsx
+++ pages/country/[id]/index.tsx
@@ -10,7 +10,7 @@ const country  = ({country}:any) => {
       <div>
 
       <Link href="/"><a className={styles.back_button}> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+  <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
 </svg> Back</a></Link>
       <div className={styles.single_page}>
       <img src={country[0].flags.svg} alt="" width="400" height="300"/>
