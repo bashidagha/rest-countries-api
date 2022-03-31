368fe18000ab5f712fd849cec965395a92a2adea
Farshid Karimi <farshidk49@gmail.com>
2022-03-31 14:57:33 +0430

    dark mode avtivated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,23 @@
 import '../styles/globals.css'
+import '../styles/globals_black.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/Navbar'
 import NextNProgress from "nextjs-progressbar";
+import {DarkenProvider} from '../context/state'
+import { useDark } from "../context/state";
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+
   return(
 <>
-  <NextNProgress />
-  <Navbar />
- <Component {...pageProps} />
+<DarkenProvider>
+      <div style={{height:"100%"}}>
+          <NextNProgress />
+          <Navbar />
+          <Component {...pageProps} />
+     </div>
+  </DarkenProvider>
 </>
 )
 }
