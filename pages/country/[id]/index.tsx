6fddf3e403aa085a50602b532467e6f0c02cb63d
Farshid Karimi <farshidk49@gmail.com>
2022-03-31 10:57:34 +0430

    solve path generation2

--- pages/country/[id]/index.tsx
+++ pages/country/[id]/index.tsx
@@ -8,17 +8,19 @@ const country  = ({country}:any) => {
     return(
 
       <div>
-      <Link href="/"><a className={styles.back_button}> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-      <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+      <Link href="/"><a className={styles.back_button} style={{backgroundColor:"white"}}> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+      <path d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
       </svg> Back</a></Link>
+
+
       <div className={styles.single_page}>
-      <img src={country[0].flags.svg} alt="" width="400" height="300"/>
+      <img src={country[0].flags.svg} alt=""/>
 
       <div>
 
       <h1>{country[0].name.common}</h1>
       <div style={{display:"flex"}}>
-      <div>
+      <div style={{marginRight:"1rem"}}>
       {country[0].name.nativeName?<p><span>Native Name: </span>{country[0].name.nativeName[Object.keys(country[0].name.nativeName)[0]].common}</p>:<p/>}
       <p><span>Population: </span>{country[0].population.toLocaleString('en-US')}</p>
       <p><span>Region: </span>{country[0].region}</p>
