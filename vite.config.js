a19689d2776c8d35f1e1118428f213f807a880a5
bashidagha <farshidk49@gmail.com>
2025-02-12 14:02:37 +0330

    feat: pwa cli for generate assets

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,28 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
     plugins: [
         react(),
-        VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
+        VitePWA({
+            registerType: "prompt",
+            devOptions: { enabled: true },
+            workbox: { globPatterns: ["**/*.{js,css,html,ico,png,svg}"] },
+            manifest: {
+                name: "Countries List",
+                short_name: "Countries",
+                description: "Countries List",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
     ],
 });
